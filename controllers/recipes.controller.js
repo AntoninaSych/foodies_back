920d69f0b745d5673b7a9d2c4167e8d7c8de9029
Ivan Dovhal <limetrack@gmail.com>
2025-04-30 12:46:35 +0300

    get recipe by id

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,4 +1,4 @@
-import { Recipe, Area, User } from '../models/index.js';
+import { Recipe, Area, User, Ingredient } from '../models/index.js';
 import HttpError from '../helpers/HttpError.js';
 
 export const getAllRecipes = async (req, res, next) => {
@@ -25,6 +25,41 @@ export const getAllRecipes = async (req, res, next) => {
         });
 
         res.json(recipes);
+    } catch (error) {
+        next(HttpError(500, error.message));
+    }
+};
+
+export const getRecipeById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const recipe = await Recipe.findOne({
+            where: { id },
+            include: [
+                {
+                    model: Area,
+                    as: 'area',
+                    attributes: ['id', 'name'],
+                },
+                {
+                    model: User,
+                    as: 'owner',
+                    attributes: ['id', 'name', 'email'],
+                },
+                {
+                    model: Ingredient,
+                    attributes: ['id', 'name'],
+                    through: { attributes: [] },
+                },
+            ],
+        });
+
+        if (!recipe) {
+            return next(HttpError(404, 'Recipe not found'));
+        }
+
+        res.json(recipe);
     } catch (error) {
         next(HttpError(500, error.message));
     }
